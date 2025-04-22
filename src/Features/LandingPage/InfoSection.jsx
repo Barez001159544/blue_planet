a4efa00b30c79b12f59afeea21ad7f5669ae06d1
barez azad <barez.001159544@gmail.com>
2025-04-22 21:51:29 +0300

    Info section background and earth image added

--- src/Features/LandingPage/InfoSection.jsx
+++ src/Features/LandingPage/InfoSection.jsx
@@ -1,9 +1,13 @@
+import earth from "../../Assets/images/earth_3d.png";
+import "./Components/InfoSection.css";
+
 const InfoSection = () => {
-    return (
-        <div>
-            <img src="" alt="" />
-        </div>
-    );
-}
+  return (
+    <div className="container">
+      <img src={earth} alt="" />
+      {/* <div className="content"></div> */}
+    </div>
+  );
+};
 
 export default InfoSection;
