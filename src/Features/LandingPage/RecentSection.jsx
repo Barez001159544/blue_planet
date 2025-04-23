146fbbe8b197a8d149b5a34bb6f51600a7de18e5
barez azad <barez.001159544@gmail.com>
2025-04-23 22:18:54 +0300

    Recent section is better

--- src/Features/LandingPage/RecentSection.jsx
+++ src/Features/LandingPage/RecentSection.jsx
@@ -1,9 +1,42 @@
 import "./Components/RecentSection.css";
+import ex from "../../Assets/images/space_bg.jpg";
+
+const blogs = [
+  {
+    id: 1,
+    title: "Exploring the Stars",
+    summary: "An introduction to stellar navigation and astrophysics.",
+    image: ex,
+  },
+  {
+    id: 2,
+    title: "Life on Mars?",
+    summary: "Could humans ever inhabit the red planet?",
+    image: ex,
+  },
+];
 
 const RecentSection = () => {
-    return (
-        <section className="recent-container"></section>
-    );
-}
+  return (
+    <section className="recent-container">
+      <h1>Recent Blogs</h1>
+      <div className="blogs">
+        {blogs.map((blog) => (
+          <article className="blog-item">
+            <img src={blog.image} alt="" />
+            <div className="blog-content">
+              <div className="blog-heading">
+                <h6>01/05</h6>
+                <div className="blog-heading-line"></div>
+              </div>
+              <h2>{blog.title}</h2>
+              <p>{blog.summary}</p>
+            </div>
+          </article>
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default RecentSection;
