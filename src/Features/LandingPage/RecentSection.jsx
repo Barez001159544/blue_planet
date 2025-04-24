448d4f3e03cdc74b33d4625a75e6f2be2b0f484a
barez azad <barez.001159544@gmail.com>
2025-04-24 18:56:36 +0300

    Recent section technologies is more appropriate

--- src/Features/LandingPage/RecentSection.jsx
+++ src/Features/LandingPage/RecentSection.jsx
@@ -1,39 +1,64 @@
 import "./Components/RecentSection.css";
-import ex from "../../Assets/Images/space_bg.jpg";
 import PrimaryButton from "../../Utils/PrimaryButton/PrimaryButton";
 
-const blogs = [
+import astronomy from "../../Assets/Images/RecentSectionImages/astronomy.jpg";
+import solar_system from "../../Assets/Images/RecentSectionImages/solar_system.jpg";
+import telescope from "../../Assets/Images/RecentSectionImages/telescope.jpg";
+import universe from "../../Assets/Images/RecentSectionImages/universe.jpg";
+
+const techs = [
   {
     id: 1,
-    title: "Exploring the Stars",
+    title: "Eyes on Asteroids",
+    link: "https://eyes.nasa.gov/apps/asteroids/#/home",
     summary:
-      "Embark on a journey through the cosmos—explore distant galaxies, uncover space mysteries, and be part of humanity’s stellar future. Embark on a journey through the cosmos—explore distant galaxies.",
-    image: ex,
+      "Explore the 3D world of Asteroids, Comets and NEOs. Learn about past and future missions, tracking and predicting orbits, and close approaches to Earth.",
+    image: solar_system,
   },
   {
     id: 2,
-    title: "Life on Mars?",
-    summary: "Could humans ever inhabit the red planet?",
-    image: ex,
+    title: "Eyes on Exoplanets",
+    link: "https://eyes.nasa.gov/apps/exo/",
+    summary:
+      "These planets outside of our solar system are known as Exoplanets. Highlighted stars are known to have, or have good prospects for, orbiting exoplanets.These planets outside of our solar system are known as Exoplanets. Highlighted stars are known to have, or have good prospects for, orbiting exoplanets.",
+    image: astronomy,
+  },
+  {
+    id: 3,
+    title: "Eyes on Earth",
+    link: "https://eyes.nasa.gov/apps/earth/#/",
+    summary:
+      "A way for you to learn about your home planet, our solar system, the universe beyond and the spacecraft exploring them.",
+    image: universe,
+  },
+  {
+    id: 4,
+    title: "Solar System Scope",
+    link: "https://www.solarsystemscope.com/",
+    summary:
+      "Solar System Scope is a model of Solar System, Night sky and Outer Space in real time, with accurate positions of objects and lots of interesting facts.",
+    image: telescope,
   },
 ];
 
 const RecentSection = () => {
   return (
     <section className="recent-container">
-      <h1>Recent Blogs</h1>
-      <div className="blogs">
-        {blogs.map((blog) => (
-          <article className="blog-item">
-            <img src={blog.image} alt="" />
-            <div className="blog-content">
-              <div className="blog-heading">
-                <h6>0{blogs.indexOf(blog) + 1}/0{blogs.length}</h6>
-                <div className="blog-heading-line"></div>
+      <h1>Recent Technologies</h1>
+      <div className="techs">
+        {techs.map((tech) => (
+          <article className="tech-item">
+            <img src={tech.image} alt="" />
+            <div className="tech-content">
+              <div className="tech-heading">
+                <h6>
+                  0{techs.indexOf(tech) + 1}/0{tech.length}
+                </h6>
+                <div className="tech-heading-line"></div>
               </div>
-              <h2>{blog.title}</h2>
-              <p>{blog.summary}</p>
-              <PrimaryButton label={"Explore"} />
+              <h2>{tech.title}</h2>
+              <p>{tech.summary}</p>
+              <PrimaryButton label={"Try It Out"} />
             </div>
           </article>
         ))}
