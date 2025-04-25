473ce5fa7e90187cf483407d3879629dfab5f15e
barez azad <barez.001159544@gmail.com>
2025-04-25 19:39:11 +0300

    Navigation is more responsive now

--- src/Features/LandingPage/NavigationBar.jsx
+++ src/Features/LandingPage/NavigationBar.jsx
@@ -1,22 +1,42 @@
+import { useState } from "react";
 import "./Components/NavigationBar.css";
 
+import close from"../../Assets/Icons/cancel.png";
+import menu from"../../Assets/Icons/menu.png";
+
 const NavigationBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
   return (
     <nav className="navbar">
-      <div className="logo">The Blue Planet</div>
-      <ul className="nav-links">
+      <div onClick={toggleMenu} className="logo">
+        The Blue Planet
+      </div>
+
+      <ul className={`nav-links ${menuOpen ? "open" : ""}`}>
         <li>
-          <a href="#home">Home</a>
+          <a href="#home" onClick={() => setMenuOpen(false)}>
+            Home
+          </a>
         </li>
         <li>
-          <a href="#info">Our Planet</a>
+          <a href="#info" onClick={() => setMenuOpen(false)}>
+            Our Planet
+          </a>
         </li>
         <li>
-          <a href="#recent">Recent Techs</a>
+          <a href="#recent" onClick={() => setMenuOpen(false)}>
+            Recent Techs
+          </a>
         </li>
         <li>
-          <a href="#footer">Get in Touch</a>
+          <a href="#footer" onClick={() => setMenuOpen(false)}>
+            Get in Touch
+          </a>
         </li>
+        <img className = "close" onClick={toggleMenu} src={close} alt="" />
       </ul>
     </nav>
   );
