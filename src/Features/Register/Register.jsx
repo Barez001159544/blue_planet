343e7383767869409e3d7969c86a2959db6bf5ba
barez azad <barez.001159544@gmail.com>
2025-04-29 22:39:30 +0300

    Login container created

--- src/Features/Register/Register.jsx
+++ src/Features/Register/Register.jsx
@@ -3,31 +3,32 @@ import IconButton from "../../Utils/IconButton/IconButton";
 import "./Components/Register.css";
 
 const Register = () => {
+  const btnIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="2"
+      stroke="black"
+      class="size-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+      />
+    </svg>
+  );
   return (
     <section className="register-section">
       <div className="image-container">
-        <IconButton
-          icon={
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="2"
-              stroke="black"
-              class="size-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
-              />
-            </svg>
-          }
-        />
+        <IconButton icon={btnIcon} />
       </div>
-      <div>
-        <h3>Get in w/ Google</h3>
-        <button>GOOGLE</button>
+      <div className="content-container">
+        <div className="in-content-container">
+          <h3>Get in w/ Google</h3>
+          <button>GOOGLE</button>
+        </div>
       </div>
     </section>
   );
