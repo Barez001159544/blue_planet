d927058a26661a94ec4c6bd0ba734e71511b23c1
barez azad <barez.001159544@gmail.com>
2025-04-29 22:24:06 +0300

    Icon button is now customizable

--- src/Features/Register/Register.jsx
+++ src/Features/Register/Register.jsx
@@ -6,7 +6,24 @@ const Register = () => {
   return (
     <section className="register-section">
       <div className="image-container">
-        <IconButton />
+        <IconButton
+          icon={
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="2"
+              stroke="black"
+              class="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+              />
+            </svg>
+          }
+        />
       </div>
       <div>
         <h3>Get in w/ Google</h3>
