a01a34ef41c77b43f7ab533da8ce38ee1e4e15f0
barez azad <barez.001159544@gmail.com>
2025-05-02 17:46:56 +0300

    Animated primary button is based on condition

--- src/Features/HomePage/HomeMenu.jsx
+++ src/Features/HomePage/HomeMenu.jsx
@@ -2,15 +2,15 @@ import "./Components/HomeMenu.css";
 import logo from "../../Assets/Images/logo.png";
 import PrimaryButton from "../../Utils/PrimaryButton/PrimaryButton";
 const HomeMenu = () => {
-    return (
-        <nav className="home-menu">
-            <img className= "logo" src={logo} alt="The Blue Planet"/>
-            <div className="navigation">
-                <a href="#">Explore</a>
-                <a href="#">Profile</a>
-            </div>
-                <PrimaryButton label={"UPLOAD"}/>
-        </nav>
-    );
-}
+  return (
+    <nav className="home-menu">
+      <img className="logo" src={logo} alt="The Blue Planet" />
+      <div className="navigation">
+        <a href="#">Explore</a>
+        <a href="#">Profile</a>
+      </div>
+      <PrimaryButton label={"UPLOAD"} animated={true} />
+    </nav>
+  );
+};
 export default HomeMenu;
--- src/Utils/PrimaryButton/PrimaryButton.jsx
+++ src/Utils/PrimaryButton/PrimaryButton.jsx
@@ -1,6 +1,8 @@
 import "./PrimaryButton.css";
-const PrimaryButton = ({ label, onClick }) => {
-  return <button className="primary-button" onClick={onClick}>{label}</button>;
+
+const PrimaryButton = ({ label, onClick, animated = false }) => {
+  const className = `primary-button ${animated ? "animated" : ""}`;
+  return <button className={className} onClick={onClick}>{label}</button>;
 };
 
 export default PrimaryButton;
