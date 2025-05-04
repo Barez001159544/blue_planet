45e1225bd3fdd10e3cd6719b6ac2cb26403c3143
barez azad <barez.001159544@gmail.com>
2025-05-04 21:00:53 +0300

    Minor custom button edits

--- src/Utils/IconButton/IconButton.jsx
+++ src/Utils/IconButton/IconButton.jsx
@@ -1,10 +1,9 @@
-import "./IconButton.css";
 const IconButton = ({ icon, onClick, style }) => {
   return (
     <button
       type="button"
       onClick={onClick}
-      class={`text-blue-700 bg-white hover:bg-gray-100 focus:outline-none font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-gray-200 dark:hover:bg-gray-300 ${style}`}
+      class={`text-blue-700 bg-white hover:bg-gray-100 focus:outline-none font-medium rounded-full text-sm p-2.5 outline-none border-none text-center inline-flex items-center me-2 dark:bg-gray-200 dark:hover:bg-gray-300 ${style}`}
     >{icon}<span class="sr-only">Back</span>
     </button>
   );
